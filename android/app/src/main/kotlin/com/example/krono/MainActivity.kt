ce5abb91dc5c84d1526ae02d96e24b64f03a0270
Sahil Sonawane <iamsahilsonawane@gmail.com>
2023-03-06 20:19:24 +0530

    init: add base project

---
+++ android/app/src/main/kotlin/com/example/krono/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.krono
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Krono - Secure file storage',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Krono")),
+    );
+  }
+}
